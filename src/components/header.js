2c42ceccfb70885a5eb700f7889267ebe71cef4a
Arjun <arjunsaini007@gmail.com>
2019-03-13 15:53:12 +0530

    local commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,20 @@
-import {Text} from 'react-native';
-import React from 'react'; 
+import { Text, StyleSheet } from 'react-native';
+import React from 'react';
 
-const Header = () => (
-     <Text> Albums!</Text>
-     );
+const Header = () => {
+
+    return <Text style={styles.bigBlue}> Albums sdfsfsd!</Text>;
+};
+
+const styles = StyleSheet.create({
+    bigBlue: {
+      color: 'blue',
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    red: {
+      color: 'red',
+    },
+  });
 
 export default Header;
