b159117423ccd2f7588752f5f7156bc9a8db7aa4
Arjun <arjunsaini007@gmail.com>
2019-03-15 18:40:43 +0530

    localc ommit

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return (<View>
-        <Text> {props.album.title}</Text>
-    </View>
-    );
+        <Card>
+            <CardSection>
+                <Image
+                    style={{ width: 66, height: 58 }}
+                    source={{ uri: props.album.image}}
+                />
+                <View style={{ margin:10 }}>
+                    <Text>{props.album.title}
+                    </Text>
+                    <Text style={{ marginTop:10 }} >{props.album.artist}
+                    </Text>
+                </View>
+            </CardSection>
+        </Card>
+    </View>);
 }
 
 export default AlbumDetail; 
---
+++ src/components/CardSection.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+const CardSection = (props) => {
+    return (<View style={styles.containerStyle}>
+        {props.children}
+        </View>);
+};
+
+const styles = {
+    containerStyle: {
+        padding: 5,
+        borderBottomWidth: 0,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
+    }
+}
+
+export default CardSection;
